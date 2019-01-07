40a0cc63f54139469d37a25de5edaf318fc261a8
AZERBOOS <cool.apps.box@gmail.com>
2019-01-07 20:48:20 +0100

    Update contact-process.php

--- contact/contact-process.php
+++ contact/contact-process.php
@@ -1,7 +1,7 @@
 <?php
 
 	
-	//$address = "example@themeforest.net";
+	//$address = "example@gmail.com";
 	$address = "hoangngocanh2495@gmail.com";
 
 	if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
@@ -14,9 +14,6 @@
 	
 	$e_subject = 'You\'ve been contacted by ' . $name . '.';
 
-		// Configuration option.
-		// You can change this if you feel that you need to.
-		// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
 
 	$e_body = "You have been contacted by: $name" . PHP_EOL . PHP_EOL;
 	$e_reply = "E-mail: $email" . PHP_EOL . PHP_EOL;
